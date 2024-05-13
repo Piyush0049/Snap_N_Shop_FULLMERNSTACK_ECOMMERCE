1ef2817484dc309d19f0a97fc5dbbcd7cad86197
Piyush0049 <157375019+Piyush0049@users.noreply.github.com>
2024-05-13 21:44:55 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,9 +8,11 @@ const bodyparser = require("body-parser");
 const payment = require("./routes/paymentroute")
 const fileupload = require("express-fileupload");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 const path = require("path");
 
+app.use(cors());
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyparser.urlencoded({extended : true}))
