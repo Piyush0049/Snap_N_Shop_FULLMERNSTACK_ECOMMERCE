669a0918cc507432e79b307c60d6c5751b8a0b1d
Piyush0049 <157375019+Piyush0049@users.noreply.github.com>
2024-05-14 19:50:58 +0530

    Update paymentroute.js

--- backend/routes/paymentroute.js
+++ backend/routes/paymentroute.js
@@ -3,6 +3,6 @@ const isauthenticated = require("../middleware/auth")
 const authorizeroles = require("../middleware/authwork");
 const { processpayment, getstripeapikey } = require("../controller/paymentcontroller");
 const router = express.Router();
-router.route("/payment/process").post(isauthenticated, processpayment);
+router.route("/payment/process").post(processpayment);
 router.route("/stripeapikey").get(getstripeapikey);
 module.exports = router
