dbc2c70b8d8a5c7c2fa59b9f911ea7ba612a9ea3
Piyush0049 <157375019+Piyush0049@users.noreply.github.com>
2024-05-14 15:14:32 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,9 @@ const cloudinary = require("cloudinary")
 const connecttodatabase = require("./config/database");
 dotenv.config({path:"backend/config/config.env"})
 app.use(cors({
-    origin : "https://main--golden-custard-15c962.netlify.app",
     methods : ["GET", "POST", "PATCH", "DELETE", "PUT"],
     credentials : true
 }));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://main--golden-custard-15c962.netlify.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
 connecttodatabase();
 cloudinary.config({
     cloud_name : process.env.CLOUDINARY_NAME,
