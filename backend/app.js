a7f40be25bb79b5957e66da3a63bd2ea8173a614
Piyush0049 <157375019+Piyush0049@users.noreply.github.com>
2024-05-14 15:15:01 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,17 +13,9 @@ const app = express();
 const path = require("path");
 
 app.use(cors({
-    origin : "https://main--golden-custard-15c962.netlify.app",
     methods : ["GET", "POST", "PATCH", "DELETE", "PUT"],
     credentials : true
 }));
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://main--golden-custard-15c962.netlify.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    next();
-});
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyparser.urlencoded({extended : true}))
