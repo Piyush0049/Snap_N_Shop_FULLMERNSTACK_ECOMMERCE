2a9f8bc64bddb74f74d5161f02b0c6b99cb74b49
Piyush0049 <157375019+Piyush0049@users.noreply.github.com>
2024-05-14 16:15:21 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,16 @@
 const app = require("./app");
-const cors = require("cors");
 const dotenv = require("dotenv");
 const cloudinary = require("cloudinary")
 const connecttodatabase = require("./config/database");
-app.use(cors({
-    origin : "https://main--golden-custard-15c962.netlify.app",
-    methods : ["GET", "POST", "PATCH", "DELETE", "PUT"],
-    credentials : true
-}));
+
 dotenv.config({path:"backend/config/config.env"});
 connecttodatabase();
 cloudinary.config({
     cloud_name : process.env.CLOUDINARY_NAME,
     api_key : process.env.CLOUDINARY_API_KEY,
     api_secret : process.env.CLOUDINARY_API_SECRET
+});
 
-})
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT, () => {
     console.log(`App working on http://localhost:${process.env.PORT}`)
-})
+});
