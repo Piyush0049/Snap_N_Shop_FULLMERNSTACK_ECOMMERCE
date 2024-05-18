7134ec710726d57c0aae9a2f8cf0b811ff7130de
Piyush0049 <157375019+Piyush0049@users.noreply.github.com>
2024-05-18 13:46:50 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 const path = require("path");
 
 app.use(cors({
-    origin : "https://main--golden-custard-15c962.netlify.app",
+    origin : "https://main--dulcet-conkies-e3a8f0.netlify.app",
     methods : ["GET", "POST", "PATCH", "DELETE", "PUT"],
     allowedHeaders: ['Content-Type'],
     credentials : true
