f4a7839cc11b0b79cffdd7ac09c7a92f53d3343e
Piyush0049 <157375019+Piyush0049@users.noreply.github.com>
2024-05-22 18:19:18 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 const path = require("path");
 
 app.use(cors({
-    origin : ["https://main--dulcet-conkies-e3a8f0.netlify.app", "http://localhost:3000", "https://ecommerce-frontend-web.vercel.app"],
+    origin : ["https://main--dulcet-conkies-e3a8f0.netlify.app", "http://localhost:3000", "https://main--ecommercefrontend123654.netlify.app"],
     methods : ["GET", "POST", "PATCH", "DELETE", "PUT"],
     allowedHeaders: ['Content-Type'],
     credentials : true
